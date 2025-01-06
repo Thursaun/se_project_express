71e7af1ece23f1b60ff8c045c59796440217910a
Thursaun <Thursaun.Canton@gmail.com>
2025-01-06 14:58:55 -0500

    001: Set up controller model route

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,13 @@
-const User = require("../models/user")
+const User = require("../models/user");
+
+const getUsers = (req, res) => {
+  User.find({})
+  .then((users) => res.send(users))
+  .catch((err) => {
+    console.error(err);
+    return res.status(500).send({ message: err.message})
+  });
+};
+
+module.exports = {getUsers};
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
+const { getUsers } = require("../controllers/users")
 
-router.get("/", () => console.log("Get users"));
+router.get("/", () => getUsers);
 router.get("/:userId", () => console.log("Get user by ID"));
 router.post("/", () => console.log("POST users"));
 
