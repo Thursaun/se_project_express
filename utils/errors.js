daec000920f22496e57608adc31b8a87231c3915
Thursaun <Thursaun.Canton@gmail.com>
2025-01-07 17:39:52 -0500

    Test 6

--- utils/errors.js
+++ utils/errors.js
@@ -3,8 +3,8 @@ const handleError = (err, res) => {
   if (err.name === "ValidationError") {
     return res.status(400).send({ message: 'Validation failed. Check input fields.'});
   }
-  if (err.name === "DocumentNotFoundError") {
-    return res.status(404).send({ message: 'Document not found.'});
+  if (err.name === "DocumentNotFoundError" || err.statusCode === 404) {
+    return res.status(404).send({ message: err.message });
   }
   if (err.name === "CastError") {
     return res.status(400).send({ message: 'Invalid ID format.'});
